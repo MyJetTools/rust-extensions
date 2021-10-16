8b3d37cc0fbbbb773d19c4a49dff7a587eaf60a7
Andrey <amigin@gmail.com>
2021-10-16 21:47:41 +0300

    Added StringBuilder

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 pub mod date_time;
 pub mod duration_utils;
 mod stop_watch;
+mod string_builder;
 mod task_completion;
 
 pub use stop_watch::StopWatch;
+pub use string_builder::StringBuilder;
 pub use task_completion::{TaskCompletion, TaskCompletionAwaiter};
---
+++ src/string_builder.rs
@@ -0,0 +1,28 @@
+use std::string::FromUtf8Error;
+
+pub struct StringBuilder {
+    buffer: Vec<u8>,
+}
+
+impl StringBuilder {
+    pub fn new() -> Self {
+        Self { buffer: Vec::new() }
+    }
+
+    pub fn append_str(&mut self, s: &str) {
+        self.buffer.extend(s.as_bytes())
+    }
+
+    pub fn append_line(&mut self, s: &str) {
+        self.buffer.extend(s.as_bytes());
+        self.buffer.push(b'\n');
+    }
+
+    pub fn append_char(&mut self, c: char) {
+        self.buffer.push(c as u8)
+    }
+
+    pub fn to_string_utf8(self) -> Result<String, FromUtf8Error> {
+        return String::from_utf8(self.buffer);
+    }
+}
