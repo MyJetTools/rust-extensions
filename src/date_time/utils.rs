b66e3950811e74b724ad420fce6433efa70ed741
Andrey <amigin@gmail.com>
2021-11-07 22:40:48 +0200

    Now we Parse ISO DateTime with microseconds precision

--- src/date_time/utils.rs
+++ src/date_time/utils.rs
@@ -5,38 +5,87 @@ use chrono::{DateTime, Utc};
 use chrono::NaiveDate;
 
 const START_ZERO: u8 = '0' as u8;
+const START_NINE: u8 = '9' as u8;
 
 pub fn parse_iso_string(src: &[u8]) -> Option<i64> {
-    let year = (src[0] - START_ZERO) as i32 * 1000
-        + (src[1] - START_ZERO) as i32 * 100
-        + (src[2] - START_ZERO) as i32 * 10
-        + (src[3] - START_ZERO) as i32;
+    let year = parse_four_digits(&src[0..4])?;
 
-    let month = (src[5] - START_ZERO) as u32 * 10 + (src[6] - START_ZERO) as u32;
+    let month = parse_two_digits(&src[5..7])?;
 
-    let day = (src[8] - START_ZERO) as u32 * 10 + (src[9] - START_ZERO) as u32;
+    let day = parse_two_digits(&src[8..10])?;
 
-    let hour = (src[11] - START_ZERO) as u32 * 10 + (src[12] - START_ZERO) as u32;
+    let hour = parse_two_digits(&src[11..13])?;
 
-    let min = (src[14] - START_ZERO) as u32 * 10 + (src[15] - START_ZERO) as u32;
+    let min = parse_two_digits(&src[14..16])?;
 
-    let sec = (src[17] - START_ZERO) as u32 * 10 + (src[18] - START_ZERO) as u32;
+    let sec = parse_two_digits(&src[17..19])?;
+
+    let date_time = NaiveDate::from_ymd(year, month, day).and_hms_milli(hour, min, sec, 0);
+
+    let result = date_time.timestamp_millis() * 1000;
 
     if src.len() <= 19 {
-        let date_time = NaiveDate::from_ymd(year, month, day).and_hms_milli(hour, min, sec, 0);
-        return Some(date_time.timestamp_millis() * 1000);
+        return Some(result);
     }
-    let msec = (src[20] - START_ZERO) as u32 * 100
-        + (src[21] - START_ZERO) as u32 * 10
-        + (src[22] - START_ZERO) as u32;
+    let microsec = parse_microseconds(&src[20..]);
 
-    let date_time = NaiveDate::from_ymd(year, month, day).and_hms_milli(hour, min, sec, msec);
+    Some(result + microsec)
+}
 
-    Some(date_time.timestamp_millis() * 1000)
+#[inline]
+fn parse_number(src: u8) -> Option<i32> {
+    if src < START_ZERO || src > START_NINE {
+        return None;
+    }
+
+    return Some((src - START_ZERO) as i32);
+}
+
+#[inline]
+fn parse_four_digits(src: &[u8]) -> Option<i32> {
+    let result = parse_number(src[0])? * 1000
+        + parse_number(src[1])? * 100
+        + parse_number(src[2])? * 10
+        + parse_number(src[3])?;
+
+    Some(result)
+}
+
+#[inline]
+fn parse_two_digits(src: &[u8]) -> Option<u32> {
+    let result = parse_number(*src.get(0)?)? * 10 + parse_number(*src.get(1)?)?;
+    Some(result as u32)
+}
+
+#[inline]
+fn parse_microseconds(src: &[u8]) -> i64 {
+    let mut multiplier: i32 = 100000;
+
+    let mut result: i32 = 0;
+
+    for b in src {
+        let b = parse_number(*b);
+
+        if b.is_none() {
+            return result as i64;
+        }
+
+        result += b.unwrap() * multiplier;
+
+        if multiplier == 1 {
+            break;
+        }
+
+        multiplier /= 10;
+    }
+
+    result as i64
 }
 
 #[cfg(test)]
 mod tests {
+    use crate::date_time::DateTimeAsMicroseconds;
+
     use super::*;
 
     #[test]
@@ -45,6 +94,34 @@ mod tests {
         let result = parse_iso_string(src.as_bytes());
 
         assert_eq!(1619371843605000, result.unwrap());
+    }
+
+    #[test]
+    pub fn test_parse_iso_string_and_back() {
+        let src = "2021-04-25T17:30:43.602432";
+        let micros = parse_iso_string(src.as_bytes()).unwrap();
+
+        let dt = DateTimeAsMicroseconds::new(micros);
+
+        let dest = dt.to_rfc3339();
+
+        assert_eq!(src, &dest[0..26]);
+    }
+
+    #[test]
+    pub fn test_parse_microseconds() {
+        let src = "605Z";
+        let result = parse_microseconds(src.as_bytes());
+
+        assert_eq!(605000, result);
+    }
+
+    #[test]
+    pub fn test_parse_microseconds_full_case() {
+        let src = "605243";
+        let result = parse_microseconds(src.as_bytes());
+
+        assert_eq!(605243, result);
     }
 }
 
