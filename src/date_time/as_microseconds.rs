32bda3df48b77fa46947e893b3388667ae86d554
amigin <amigin@gmail.com>
2022-05-27 11:50:07 +0200

    Covered Add_seconds etc for DateTimeAsMicroseconds

--- src/date_time/as_microseconds.rs
+++ src/date_time/as_microseconds.rs
@@ -79,4 +79,44 @@ mod tests {
         let before = DateTimeAsMicroseconds::parse_iso_string("2021-04-25T17:30:00.000Z").unwrap();
         assert_eq!(3, now.seconds_before(before));
     }
+
+    #[test]
+    fn test_add_seconds() {
+        let mut now = DateTimeAsMicroseconds::parse_iso_string("2021-04-25T17:30:03.000Z").unwrap();
+
+        now.add_seconds(1);
+
+        let result = DateTimeAsMicroseconds::parse_iso_string("2021-04-25T17:30:04.000Z").unwrap();
+        assert_eq!(now.unix_microseconds, result.unix_microseconds)
+    }
+
+    #[test]
+    fn test_add_minutes() {
+        let mut now = DateTimeAsMicroseconds::parse_iso_string("2021-04-25T17:30:03.000Z").unwrap();
+
+        now.add_minutes(1);
+
+        let result = DateTimeAsMicroseconds::parse_iso_string("2021-04-25T17:31:03.000Z").unwrap();
+        assert_eq!(now.unix_microseconds, result.unix_microseconds)
+    }
+
+    #[test]
+    fn test_add_hours() {
+        let mut now = DateTimeAsMicroseconds::parse_iso_string("2021-04-25T17:30:03.000Z").unwrap();
+
+        now.add_hours(1);
+
+        let result = DateTimeAsMicroseconds::parse_iso_string("2021-04-25T18:30:03.000Z").unwrap();
+        assert_eq!(now.unix_microseconds, result.unix_microseconds)
+    }
+
+    #[test]
+    fn test_add_days() {
+        let mut now = DateTimeAsMicroseconds::parse_iso_string("2021-04-25T17:30:03.000Z").unwrap();
+
+        now.add_days(1);
+
+        let result = DateTimeAsMicroseconds::parse_iso_string("2021-04-26T17:30:03.000Z").unwrap();
+        assert_eq!(now.unix_microseconds, result.unix_microseconds)
+    }
 }
