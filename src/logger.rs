44387fa24774c0afc08bed8ca93580bcbe803290
amigin <amigin@gmail.com>
2022-06-24 16:59:50 +0300

    Added write_warning to logger

--- src/logger.rs
+++ src/logger.rs
@@ -1,5 +1,6 @@
 pub trait Logger {
     fn write_info(&self, process: String, message: String, ctx: Option<String>);
+    fn write_warning(&self, process: String, message: String, ctx: Option<String>);
     fn write_error(&self, process: String, message: String, ctx: Option<String>);
     fn write_fatal_error(&self, process: String, message: String, ctx: Option<String>);
 }
