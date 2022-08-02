aef912101f23c62fe04f25bc2083ec4411838770
amigin <amigin@gmail.com>
2022-08-02 19:54:17 +0300

    Logger ctx now is KeyValue

--- src/logger.rs
+++ src/logger.rs
@@ -1,6 +1,13 @@
+use std::collections::HashMap;
+
 pub trait Logger {
-    fn write_info(&self, process: String, message: String, ctx: Option<String>);
-    fn write_warning(&self, process: String, message: String, ctx: Option<String>);
-    fn write_error(&self, process: String, message: String, ctx: Option<String>);
-    fn write_fatal_error(&self, process: String, message: String, ctx: Option<String>);
+    fn write_info(&self, process: String, message: String, ctx: Option<HashMap<String, String>>);
+    fn write_warning(&self, process: String, message: String, ctx: Option<HashMap<String, String>>);
+    fn write_error(&self, process: String, message: String, ctx: Option<HashMap<String, String>>);
+    fn write_fatal_error(
+        &self,
+        process: String,
+        message: String,
+        ctx: Option<HashMap<String, String>>,
+    );
 }
