191cfca123b34a7c1b0e0a73c9cb004a03cd7ca6
amigin <amigin@gmail.com>
2022-08-07 23:45:39 +0300

    Added completed to task_completion

--- src/task_completion/task_completion_awaiter.rs
+++ src/task_completion/task_completion_awaiter.rs
@@ -6,27 +6,37 @@ pub enum CompletionEvent<OkResult, ErrorResult> {
     Error(ErrorResult),
 }
 
-pub struct TaskCompletionAwaiter<OkResult, ErrorResult> {
-    pub receiver: Receiver<CompletionEvent<OkResult, ErrorResult>>,
+pub enum TaskCompletionAwaiter<OkResult, ErrorResult> {
+    Awaiting(Receiver<CompletionEvent<OkResult, ErrorResult>>),
+    Completed(Result<OkResult, ErrorResult>),
 }
 
 impl<OkResult, ErrorResult> TaskCompletionAwaiter<OkResult, ErrorResult> {
     pub fn new(receiver: Receiver<CompletionEvent<OkResult, ErrorResult>>) -> Self {
-        Self { receiver }
+        Self::Awaiting(receiver)
+    }
+
+    pub fn create_completed(result: Result<OkResult, ErrorResult>) -> Self {
+        Self::Completed(result)
     }
 
     pub async fn get_result(self) -> Result<OkResult, ErrorResult> {
-        let result = self.receiver.await;
+        match self {
+            TaskCompletionAwaiter::Awaiting(receiver) => {
+                let result = receiver.await;
 
-        match result {
-            Ok(result) => match result {
-                CompletionEvent::Ok(ok) => return Ok(ok),
-                CompletionEvent::Error(err) => return Err(err),
-            },
-            Err(error) => panic!(
-                "Can not recivev result for a task completion. Err: {:?}",
-                error
-            ),
+                match result {
+                    Ok(result) => match result {
+                        CompletionEvent::Ok(ok) => return Ok(ok),
+                        CompletionEvent::Error(err) => return Err(err),
+                    },
+                    Err(error) => panic!(
+                        "Can not recivev result for a task completion. Err: {:?}",
+                        error
+                    ),
+                }
+            }
+            TaskCompletionAwaiter::Completed(completed) => completed,
         }
     }
 }
