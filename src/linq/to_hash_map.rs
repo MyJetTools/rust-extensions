9462c081a8d16696a0b2181ed757bf92ad717e78
amigin <amigin@gmail.com>
2022-09-10 18:51:55 +0300

    Fixed linq to_has_map

--- src/linq/to_hash_map.rs
+++ src/linq/to_hash_map.rs
@@ -1,30 +1,21 @@
 use std::collections::HashMap;
 
 pub trait ToHashMap<TSrcValue, TKey, TValue> {
-    fn to_hash_map(
-        self,
-        get_key: fn(&TSrcValue) -> TKey,
-        get_value: fn(TSrcValue) -> TValue,
-    ) -> HashMap<TKey, TValue>;
+    fn to_hash_map(self, get_key_value: fn(TSrcValue) -> (TKey, TValue)) -> HashMap<TKey, TValue>;
 }
 
 impl<TSrcValue, TKey, TValue> ToHashMap<TSrcValue, TKey, TValue> for Vec<TSrcValue>
 where
     TKey: std::cmp::Eq + core::hash::Hash + Clone,
 {
-    fn to_hash_map(
-        self,
-        get_key: fn(&TSrcValue) -> TKey,
-        get_value: fn(TSrcValue) -> TValue,
-    ) -> HashMap<TKey, TValue> {
-        to_hash_map(self.into_iter(), get_key, get_value)
+    fn to_hash_map(self, get_key_value: fn(TSrcValue) -> (TKey, TValue)) -> HashMap<TKey, TValue> {
+        to_hash_map(self.into_iter(), get_key_value)
     }
 }
 
 pub fn to_hash_map<TSrcValue, TKey, TValue, TIter: Iterator<Item = TSrcValue>>(
     items: TIter,
-    get_key: fn(&TSrcValue) -> TKey,
-    get_value: fn(TSrcValue) -> TValue,
+    get_key_value: fn(TSrcValue) -> (TKey, TValue),
 ) -> HashMap<TKey, TValue>
 where
     TKey: std::cmp::Eq + core::hash::Hash + Clone,
@@ -32,7 +23,8 @@ where
     let mut result = HashMap::new();
 
     for item in items {
-        result.insert(get_key(&item), get_value(item));
+        let (key, value) = get_key_value(item);
+        result.insert(key, value);
     }
 
     result
@@ -46,7 +38,7 @@ mod tests {
     fn test_to_hash_map() {
         let items = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-        let result = to_hash_map(items.into_iter(), |item| item.to_string(), |item| item);
+        let result = to_hash_map(items.into_iter(), |item| (item.to_string(), item));
 
         assert_eq!(result.len(), 10);
 
