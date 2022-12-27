8f0a577340a9a82e91c15ae99cad6690ab046add
amigin <amigin@gmail.com>
2022-12-27 19:57:34 +0200

    LazyVec now has with_capacity()

--- src/lazy/lazy_vec.rs
+++ src/lazy/lazy_vec.rs
@@ -1,15 +1,30 @@
 pub struct LazyVec<T> {
     pub result: Option<Vec<T>>,
+    pub capacity: Option<usize>,
 }
 
 impl<T> LazyVec<T> {
     pub fn new() -> Self {
-        Self { result: None }
+        Self {
+            result: None,
+            capacity: None,
+        }
+    }
+
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            result: None,
+            capacity: Some(capacity),
+        }
     }
 
     pub fn add(&mut self, t: T) {
         if self.result.is_none() {
-            self.result = Some(Vec::new());
+            if let Some(capacity) = self.capacity {
+                self.result = Some(Vec::with_capacity(capacity));
+            } else {
+                self.result = Some(Vec::new());
+            }
         }
 
         self.result.as_mut().unwrap().push(t);
