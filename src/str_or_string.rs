e9af532f51f04c85ed4a0893251685b44b127761
amigin <amigin@gmail.com>
2023-03-17 11:22:07 +0200

    Made possible StringOrStr to slice data inside

--- src/str_or_string.rs
+++ src/str_or_string.rs
@@ -1,29 +1,75 @@
-pub enum StrOrString<'s> {
-    AsStr(&'s str),
-    AsString(String),
+pub struct StrOrString<'s> {
+    data: StrOrStringData<'s>,
+    from: Option<usize>,
+    to: Option<usize>,
 }
 
 impl<'s> StrOrString<'s> {
     pub fn crate_as_str(s: &'s str) -> Self {
-        Self::AsStr(s)
+        Self {
+            data: StrOrStringData::AsStr(s),
+            from: None,
+            to: None,
+        }
     }
+
     pub fn crate_as_string(s: String) -> Self {
-        Self::AsString(s)
+        Self {
+            data: StrOrStringData::AsString(s),
+            from: None,
+            to: None,
+        }
+    }
+    pub fn slice_it(&mut self, from: Option<usize>, to: Option<usize>) {
+        self.from = from;
+        self.to = to;
+    }
+
+    fn has_data_to_cut(&self) -> bool {
+        self.from.is_some() || self.to.is_some()
     }
 
     pub fn as_str(&'s self) -> &'s str {
-        match self {
-            Self::AsStr(s) => s,
-            Self::AsString(s) => s.as_str(),
-        }
+        let result = match &self.data {
+            StrOrStringData::AsStr(s) => s,
+            StrOrStringData::AsString(s) => s.as_str(),
+        };
+
+        cut_data(result, self.from, self.to)
     }
 
     pub fn to_string(self) -> String {
-        match self {
-            Self::AsStr(s) => s.to_string(),
-            Self::AsString(s) => s,
+        let has_data_to_cut = self.has_data_to_cut();
+        match self.data {
+            StrOrStringData::AsStr(s) => cut_data(s, self.from, self.to).to_string(),
+            StrOrStringData::AsString(s) => {
+                if has_data_to_cut {
+                    cut_data(&s, self.from, self.to).to_string()
+                } else {
+                    s
+                }
+            }
         }
     }
+}
+
+fn cut_data(src: &str, src_from: Option<usize>, src_to: Option<usize>) -> &str {
+    if let Some(from) = src_from {
+        if let Some(to) = src_to {
+            return &src[from..to];
+        } else {
+            return &src[from..];
+        }
+    }
+
+    if let Some(to) = src_to {
+        return &src[..to];
+    }
+    src
+}
+pub enum StrOrStringData<'s> {
+    AsStr(&'s str),
+    AsString(String),
 }
 
 impl Into<String> for StrOrString<'_> {
@@ -44,12 +90,33 @@ pub trait IntoStringOrStr<'s> {
 
 impl<'s> IntoStringOrStr<'s> for String {
     fn into_string_or_str(self) -> StrOrString<'s> {
-        StrOrString::AsString(self)
+        StrOrString::crate_as_string(self)
     }
 }
 
 impl<'s> IntoStringOrStr<'s> for &'s str {
     fn into_string_or_str(self) -> StrOrString<'s> {
-        StrOrString::AsStr(self)
+        StrOrString::crate_as_str(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::StrOrString;
+
+    #[test]
+    fn test_src_with_cut() {
+        let mut src = StrOrString::crate_as_str("123");
+        src.slice_it(1.into(), 2.into());
+
+        assert_eq!("2", src.as_str())
+    }
+
+    #[test]
+    fn test_string_with_cut() {
+        let mut src = StrOrString::crate_as_string("123".to_string());
+        src.slice_it(1.into(), 2.into());
+
+        assert_eq!("2", src.as_str())
     }
 }
