30c7b7b4896b8b7312240e147751e80436012a58
amigin <amigin@gmail.com>
2023-04-20 14:45:54 +0300

    Small Fixes

--- src/as_slice_or_vec/slice_or_vec_seq_reader.rs
+++ src/as_slice_or_vec/slice_or_vec_seq_reader.rs
@@ -3,27 +3,26 @@ use std::io::{Read, Seek};
 use crate::AsSliceOrVec;
 
 pub struct SliceOrVecSeqReader<'s, T: Clone> {
-    src: AsSliceOrVec<'s, T>,
+    inner: AsSliceOrVec<'s, T>,
     offset: usize,
 }
 
 impl<'s, T: Clone> SliceOrVecSeqReader<'s, T> {
     pub fn new(src: AsSliceOrVec<'s, T>) -> Self {
-        Self { src, offset: 0 }
+        Self {
+            inner: src,
+            offset: 0,
+        }
     }
 
     pub fn get_offset(&self) -> usize {
         self.offset
     }
-
-    fn shift_offset_forward(&mut self, len: usize) {
-        self.offset += len;
-    }
 }
 
 impl<'s> Read for SliceOrVecSeqReader<'s, u8> {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
-        let len = self.src.get_len();
+        let len = self.inner.get_len();
         let offset = self.get_offset();
 
         if offset >= len {
@@ -33,14 +32,14 @@ impl<'s> Read for SliceOrVecSeqReader<'s, u8> {
         let remain = len - offset;
 
         let read_len = if remain < buf.len() {
-            buf[..remain].copy_from_slice(&self.src.as_slice()[offset..]);
+            buf[..remain].copy_from_slice(&self.inner.as_slice()[offset..offset + remain]);
             remain
         } else {
-            buf.copy_from_slice(&self.src.as_slice()[offset..offset + buf.len()]);
+            buf.copy_from_slice(&self.inner.as_slice()[offset..offset + buf.len()]);
             buf.len()
         };
 
-        self.shift_offset_forward(read_len);
+        self.offset += read_len;
 
         Ok(read_len)
     }
@@ -48,42 +47,32 @@ impl<'s> Read for SliceOrVecSeqReader<'s, u8> {
 
 impl<'s> Seek for SliceOrVecSeqReader<'s, u8> {
     fn seek(&mut self, pos: std::io::SeekFrom) -> std::io::Result<u64> {
-        match pos {
+        let new_pos = match pos {
             std::io::SeekFrom::Start(value) => {
                 self.offset = value as usize;
-
-                if self.offset >= self.src.get_len() {
-                    return Err(std::io::Error::new(
-                        std::io::ErrorKind::InvalidInput,
-                        "invalid seek to overflowing position",
-                    ));
-                }
+                return Ok(value);
             }
-            std::io::SeekFrom::End(value) => {
-                let offset = self.src.get_len() as i64 + value;
+            std::io::SeekFrom::End(value) => self.inner.get_len() as i64 + value,
+            std::io::SeekFrom::Current(value) => self.offset as i64 + value,
+        };
 
-                if offset < 0 {
-                    return Err(std::io::Error::new(
-                        std::io::ErrorKind::InvalidInput,
-                        "invalid seek to a negative or overflowing position",
-                    ));
-                } else {
-                    self.offset = offset as usize;
-                }
-            }
-            std::io::SeekFrom::Current(value) => {
-                let offset = self.offset as i64 + value;
-
-                if offset < 0 {
-                    return Err(std::io::Error::new(
-                        std::io::ErrorKind::InvalidInput,
-                        "invalid seek to a negative or overflowing position",
-                    ));
-                } else {
-                    self.offset = offset as usize;
-                }
-            }
+        if new_pos < 0 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "invalid seek to a negative position",
+            ));
         }
+
+        let new_pos = new_pos as usize;
+
+        if new_pos > self.inner.get_len() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "invalid seek to a position that is larger than the length of the slice",
+            ));
+        }
+
+        self.offset = new_pos;
 
         Ok(self.offset as u64)
     }
