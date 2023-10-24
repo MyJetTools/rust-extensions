e9dfd60e709e72ce0434c596cec9700b5934d660
amigin <amigin@gmail.com>
2023-10-25 00:00:10 +0300

    feature with-tokio enables only with tokio related features are needed

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,15 @@
+#[cfg(feature = "with-tokio")]
 mod application_states;
 mod as_slice_or_vec;
 mod binary_payload_builder;
 pub mod date_time;
 pub mod duration_utils;
+#[cfg(feature = "with-tokio")]
 pub mod events_loop;
 pub mod lazy;
 pub mod linq;
 mod logger;
+#[cfg(feature = "with-tokio")]
 mod my_timer;
 mod short_string;
 pub use short_string::*;
@@ -17,18 +20,21 @@ pub mod slice_of_u8_utils;
 mod stop_watch;
 mod str_or_string;
 mod string_builder;
+#[cfg(feature = "with-tokio")]
 mod task_completion;
 
+#[cfg(feature = "with-tokio")]
+pub use application_states::*;
 pub use stop_watch::StopWatch;
 pub use string_builder::StringBuilder;
+#[cfg(feature = "with-tokio")]
 pub use task_completion::{TaskCompletion, TaskCompletionAwaiter, TaskCompletionError};
-
-pub use application_states::*;
 pub mod grouped_data;
 
 pub use as_slice_or_vec::*;
 pub use binary_payload_builder::*;
 pub use logger::*;
+#[cfg(feature = "with-tokio")]
 pub use my_timer::{MyTimer, MyTimerTick};
 pub use str_or_string::*;
 #[cfg(feature = "base64")]
