c814ee8a11439997bee1a53c31aad570f80c8f70
amigin <amigin@gmail.com>
2023-11-14 09:55:11 +0200

    Marked to_hash_map as depricated

--- src/linq/mod.rs
+++ src/linq/mod.rs
@@ -2,3 +2,21 @@ mod to_hash_map;
 pub use to_hash_map::*;
 mod to_btree_map;
 pub use to_btree_map::*;
+
+#[deprecated(note = "Please use to_hash_map or to_btree_map instead")]
+pub fn to_hash_map<TSrcValue, TKey, TValue, TIter: Iterator<Item = TSrcValue>>(
+    items: TIter,
+    get_key_value: fn(TSrcValue) -> (TKey, TValue),
+) -> std::collections::HashMap<TKey, TValue>
+where
+    TKey: std::cmp::Eq + core::hash::Hash + Clone,
+{
+    let mut result = std::collections::HashMap::new();
+
+    for item in items {
+        let (key, value) = get_key_value(item);
+        result.insert(key, value);
+    }
+
+    result
+}
