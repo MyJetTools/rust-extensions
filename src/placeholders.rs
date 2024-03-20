5589a3792baa475b9788495347f84bb5bfcc8af2
amigin <amigin@gmail.com>
2024-03-20 21:36:25 +0200

    Made placeholder iterator

--- src/placeholders.rs
+++ src/placeholders.rs
@@ -22,52 +22,73 @@ impl<'s> ContentToken<'s> {
     }
 }
 
-pub fn get_tokens_with_placeholders<'s>(content: &'s str) -> Vec<ContentToken<'s>> {
-    let mut result = Vec::new();
+const KEY_START: &str = "${";
 
-    let content = content.as_bytes();
+pub struct PlaceholdersIterator<'s> {
+    content: &'s [u8],
+    i: usize,
+    reading_key: bool,
+}
 
-    const KEY_START: &str = "${";
+impl<'s> PlaceholdersIterator<'s> {
+    pub fn new(content: &'s str) -> Self {
+        Self {
+            content: content.as_bytes(),
+            i: 0,
+            reading_key: false,
+        }
+    }
+}
 
-    let mut i = 0;
+impl<'s> Iterator for PlaceholdersIterator<'s> {
+    type Item = ContentToken<'s>;
 
-    loop {
-        let key_start = content.find_sequence_pos(KEY_START.as_bytes(), i);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i >= self.content.len() {
+            return None;
+        }
 
-        if key_start.is_none() {
-            if i < content.len() {
-                let text = std::str::from_utf8(&content[i..content.len()]).unwrap();
-                result.push(ContentToken::Text(text));
+        if !self.reading_key {
+            let key_start = self.content.find_sequence_pos(KEY_START.as_bytes(), self.i);
+
+            if key_start.is_none() {
+                let text = std::str::from_utf8(&self.content[self.i..]).unwrap();
+                self.i = self.content.len();
+                return Some(ContentToken::Text(text));
             }
 
-            break;
-        }
-        let key_start = key_start.unwrap();
+            let key_start = key_start.unwrap();
 
-        if i < key_start {
-            let text = std::str::from_utf8(&content[i..key_start]).unwrap();
-            result.push(ContentToken::Text(text));
+            if self.i < key_start {
+                let text = std::str::from_utf8(&self.content[self.i..key_start]).unwrap();
+                self.i = key_start;
+                self.reading_key = true;
+                return Some(ContentToken::Text(text));
+            } else {
+                self.reading_key = true;
+                self.i = key_start;
+            }
         }
 
-        let key_end = content.find_byte_pos(b'}', key_start);
+        let key_end = self.content.find_byte_pos(b'}', self.i);
 
         if key_end.is_none() {
-            break;
+            let text = std::str::from_utf8(&self.content[self.i..]).unwrap();
+            self.i = self.content.len();
+            return Some(ContentToken::Text(text));
         }
 
         let key_end = key_end.unwrap();
 
-        let text = std::str::from_utf8(&content[key_start + 2..key_end]).unwrap();
-        result.push(ContentToken::Placeholder(text));
-
-        i = key_end + 1;
+        let text = std::str::from_utf8(&self.content[self.i + 2..key_end]).unwrap();
+        self.i = key_end + 1;
+        self.reading_key = false;
+        Some(ContentToken::Placeholder(text))
     }
-
-    result
 }
 
 pub fn has_placeholder(src: &str, place_holder: &str) -> bool {
-    for token in get_tokens_with_placeholders(src) {
+    for token in PlaceholdersIterator::new(src) {
         if let ContentToken::Placeholder(value) = token {
             if value == place_holder {
                 return true;
@@ -80,12 +101,13 @@ pub fn has_placeholder(src: &str, place_holder: &str) -> bool {
 
 #[cfg(test)]
 mod test {
+    use crate::placeholders::PlaceholdersIterator;
 
     #[test]
     fn get_tokens_with_placeholders() {
         let src = "my secret is ${secret1} and ${secret2} is my secret";
 
-        let tokens = super::get_tokens_with_placeholders(src);
+        let tokens: Vec<_> = PlaceholdersIterator::new(src).collect();
 
         assert_eq!(tokens.len(), 5);
 
@@ -100,7 +122,7 @@ mod test {
     fn get_tokens_with_placeholder_at_the_end() {
         let src = "my secret is ${secret1} and ${secret2}";
 
-        let tokens = super::get_tokens_with_placeholders(src);
+        let tokens: Vec<_> = PlaceholdersIterator::new(src).collect();
 
         assert_eq!(tokens.len(), 4);
 
@@ -114,7 +136,7 @@ mod test {
     fn get_tokens_with_placeholders_with_placeholder_at_beginning() {
         let src = "${secret1} and ${secret2} is my secret";
 
-        let tokens = super::get_tokens_with_placeholders(src);
+        let tokens: Vec<_> = PlaceholdersIterator::new(src).collect();
 
         assert_eq!(tokens.len(), 4);
 
