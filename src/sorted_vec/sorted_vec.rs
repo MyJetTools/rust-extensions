a2abd2973676cca16c4fe18565dc3485cf1f22f7
amigin <amigin@gmail.com>
2024-10-05 13:07:41 +0300

    Added PartialEq

--- src/sorted_vec/sorted_vec.rs
+++ src/sorted_vec/sorted_vec.rs
@@ -4,7 +4,7 @@ use crate::sorted_vec::{
 
 use super::InsertIfNotExists;
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct SortedVec<TKey: Ord, TValue: EntityWithKey<TKey>> {
     items: Vec<TValue>,
     itm: std::marker::PhantomData<TKey>,
